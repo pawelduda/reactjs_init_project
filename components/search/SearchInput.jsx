90af9c1ce066f12af0628e8459864dd3a8c1b52a
pawelduda <pawelduda91@gmail.com>
2016-05-31 23:43:08 +0200

    Use axios to grab search results number via Google Custom Search API

--- components/search/SearchInput.jsx
+++ components/search/SearchInput.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class SearchInput extends React.Component {
+  constructor () {
+    super()
+
+    this.handleChange = this.handleChange.bind(this)
+    this.ajaxUrl = this.ajaxUrl.bind(this)
+  }
+
+  handleChange (e) {
+    axios
+      .get(this.ajaxUrl(e.target.value))
+      .then(function (response) {
+        console.log(response.data.searchInformation.formattedTotalResults)
+      })
+  }
+
+  ajaxUrl (searchQuery) {
+    return `https://www.googleapis.com/customsearch/v1?key=AIzaSyA7w4pR_1tc8DG3S3qzFplJEp9CAyjF1MY&cx=015538004234392094052:bcfx59rb48y&q=${searchQuery}`
+  }
+
   render () {
     return (
       <div className='form-group'>
         <label for='searchPhrase' className='control-label'>
           Search for a phrase
         </label>
-        <input name='searchPhrase' type='text'
+        <input onChange={this.handleChange}
+          name='searchPhrase' type='text'
           className='form-control input-lg'
           placeholder='Start typing to search...' />
       </div>
