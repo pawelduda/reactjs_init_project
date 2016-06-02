548a00cd824c6557cb5ae02e560955ea3b19c35c
pawelduda <pawelduda91@gmail.com>
2016-06-02 23:04:25 +0200

    Implement static components structure

--- containers/PricingContainer.jsx
+++ containers/PricingContainer.jsx
@@ -3,4 +3,18 @@ import React, { Component } from 'react'
 import PricingLevel from '../components/pricing/PricingLevel.jsx'
 
 export default class PricingContainer extends Component {
+  render () {
+    return (
+      <div>
+        <div className="row text-center">
+          <h2>Choose your pricing plan:</h2>
+        </div>
+        <div className="row text-center margin-top-15">
+          <div className="col-md-4 margin-top-15"><PricingLevel price={99} /></div>
+          <div className="col-md-4 margin-top-15"><PricingLevel price={199} /></div>
+          <div className="col-md-4 margin-top-15"><PricingLevel price={299} /></div>
+        </div>
+      </div>
+    )
+  }
 }
