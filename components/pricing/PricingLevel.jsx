69b1d9f432e00d33db1212b7ccb0b0faa97358bf
pawelduda <pawelduda91@gmail.com>
2016-06-03 20:33:01 +0200

    Format price numbers update price multipliers to reflect reality a bit better

--- components/pricing/PricingLevel.jsx
+++ components/pricing/PricingLevel.jsx
@@ -1,20 +1,28 @@
 import React, { Component, PropTypes } from 'react'
+import accounting from 'accounting'
 
 export default class PricingLevel extends Component {
   constructor () {
     super()
 
     this.price = this.price.bind(this)
+    this.formattedPrice = this.formattedPrice.bind(this)
+  }
+
+  formattedPrice () {
+    return accounting.formatMoney(this.price())
   }
 
   price () {
-    return this.props.searchResultsCount * this.props.pricingMultiplier
+    return Math.round(
+      this.props.searchResultsCount * this.props.pricingMultiplier
+    )
   }
 
   render () {
     return (
       <button className='btn btn-primary btn-lg btn-block'>
-        ${this.price()}
+        {this.formattedPrice()}
       </button>
     )
   }
