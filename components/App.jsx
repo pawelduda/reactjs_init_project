8ff2d28a0b37a6e2c8f38f4681d698f55429d0dd
pawelduda <pawelduda91@gmail.com>
2016-06-05 15:43:57 +0200

    Keep state in App component in order to make passing props down the components easier

--- components/App.jsx
+++ components/App.jsx
@@ -1,13 +1,88 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { debounce } from 'throttle-debounce'
 
 import SearchContainer from '../containers/SearchContainer.jsx'
+import PricingContainer from '../containers/PricingContainer.jsx'
 import ContactContainer from '../containers/ContactContainer.jsx'
 
 export default class App extends Component {
+  constructor () {
+    super()
+
+    this.defaultState = {
+      searchResultsMessage: '',
+      searchResultsCount: 0,
+      error: false
+    }
+    this.state = this.defaultState
+
+    this.onUserInput = debounce(500, this.onUserInput.bind(this))
+  }
+
+  onUserInput (searchQuery) {
+    if (searchQuery.length < 3) {
+      this.setState(this.defaultState)
+      return
+    }
+
+    this.setState(
+      {
+        ...this.defaultState,
+        searchResultsMessage: '...'
+      }
+    )
+
+    axios
+      .get(this.ajaxUrl(searchQuery))
+      .then((response) => this.handleSearchSuccess(response))
+      .catch((response) => this.handleSearchError(response))
+  }
+
+  handleSearchSuccess (response) {
+    const searchResultsCount = response.data.searchInformation.formattedTotalResults
+
+    this.setState(
+      {
+        ...this.defaultState,
+        searchResultsMessage: `Search results count: ${searchResultsCount}`,
+        searchResultsCount: Number(searchResultsCount.replace(/,/g, ''))
+      }
+    )
+  }
+
+  handleSearchError (response) {
+    if (response.status === 403) {
+      this.setState(
+        {
+          ...this.defaultState,
+          error: true,
+          searchResultsMessage: 'Google API daily limit exceeded. Try again later.'
+        }
+      )
+    } else {
+      this.setState(
+        {
+          ...this.defaultState,
+          error: true,
+          searchResultsMessage: 'An error occurred. Try again later.'
+        }
+      )
+    }
+  }
+
+  ajaxUrl (searchQuery) {
+    return `https://www.googleapis.com/customsearch/v1?key=AIzaSyDcQO-ogNX2SPtTCmmRpvBKuvMErrEsouk&cx=015538004234392094052:bcfx59rb48y&q=${searchQuery}`
+  }
+
   render () {
     return (
       <div className='container margin-top-15'>
-        <SearchContainer />
+        <SearchContainer
+          onUserInput={this.onUserInput} error={this.state.error}
+          searchResultsMessage={this.state.searchResultsMessage} />
+        <PricingContainer
+          searchResultsCount={this.state.searchResultsCount} />
         <ContactContainer />
       </div>
     )
--- containers/PricingContainer.jsx
+++ containers/PricingContainer.jsx
@@ -8,23 +8,27 @@ export default class PricingContainer extends Component {
   }
 
   render () {
-    return (
-      <div>
-        <div className='row text-center'>
-          <h2>Choose your pricing plan:</h2>
+    if (this.props.searchResultsCount > 0) {
+      return (
+        <div>
+          <div className='row text-center'>
+            <h2>Choose your pricing plan:</h2>
+          </div>
+          <div className='row text-center margin-top-15'>
+            <div className='col-md-4 margin-top-15'>
+              <PricingLevel searchResultsCount={this.props.searchResultsCount} pricingMultiplier={0.001} />
+            </div>
+            <div className='col-md-4 margin-top-15'>
+              <PricingLevel searchResultsCount={this.props.searchResultsCount} pricingMultiplier={0.003} />
+            </div>
+            <div className='col-md-4 margin-top-15'>
+              <PricingLevel searchResultsCount={this.props.searchResultsCount} pricingMultiplier={0.004} />
+            </div>
+          </div>
         </div>
-        <div className='row text-center margin-top-15'>
-          <div className='col-md-4 margin-top-15'>
-            <PricingLevel searchResultsCount={this.props.searchResultsCount} pricingMultiplier={0.001} />
-          </div>
-          <div className='col-md-4 margin-top-15'>
-            <PricingLevel searchResultsCount={this.props.searchResultsCount} pricingMultiplier={0.003} />
-          </div>
-          <div className='col-md-4 margin-top-15'>
-            <PricingLevel searchResultsCount={this.props.searchResultsCount} pricingMultiplier={0.004} />
-          </div>
-        </div>
-      </div>
-    )
+      )
+    } else {
+      return null
+    }
   }
 }
