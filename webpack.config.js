5b32fd2d0a41485abc3c1314d170707f6d0f0cbe
pawelduda <pawelduda91@gmail.com>
2016-06-11 15:36:16 +0200

    Install babel stage-0 preset

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         loader: 'babel',
 
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-0']
         }
       }
     ]
